4d6bcb5d7adcd90923ea48f492aa3637facd7b6b
unknown <67815810+unknown-01-hacker@users.noreply.github.com>
2020-10-16 18:00:27 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -23,9 +23,9 @@ class Translation(object):
     SHOW_CUSTOM_THUMB_NAIL = "<b>🔰Permanent Thumbnail🔰</b> \n\n<b>"
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
     KL35_THUMBNAIL_PHOTO = "https://telegra.ph/file/1ee9fd2c4b6a6b3c39262.jpg"
-    ABOUT_ME = """<b>🚫 group:__</b> \n\n<b>🔰 owner Name: @DeathCaptureBoy</b> \n\n<b>🛡️ Creator: @KL_noob</b> \n\n<b>🔸 Language: Python3</b> \n\n<b>🔸 Library : <a href="https://docs.pyrogram.org/">Pyrogram asyncio 0.16.1</a></b> \n\n<b>🔸 Source Code : 👉 <a href="https://bit.ly/2AVFsoD">Click Here</a></b> \n\n<b>"""
+    ABOUT_ME = """<b>🚫 group:__</b> \n\n<b>🔰 owner Name: </b> \n\n<b>🛡️ Creator: @KL_noob</b> \n\n<b>🔸 Language: Python3</b> \n\n<b>🔸 Library : <a href="https://docs.pyrogram.org/">Pyrogram asyncio 0.16.1</a></b> \n\n<b>🔸 Source Code : 👉 <a href="https://bit.ly/2AVFsoD">Click Here</a></b> \n\n<b>"""
     HELP_USER = """ഇത് അത്രക്ക് പാടൊന്നുമല്ലടെ.
-    
+    KL14_THUMBNAIL_PHOTO = "https://telegra.ph/file/1ee9fd2c4b6a6b3c39262.jpg"
 1. ഒരു Telegram File Send ചെയ്യുക.
 2. Thumbnail തരുക.(നീ ഒരു പ്രാവശ്യം തന്നാൽ മതി)
 3. ആ ഫയലിന് Reply ആയി /rename NewName.Extension എന്ന് തരുക. \n\n<b>"""
