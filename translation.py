d331fb6a1707e1cbc8810a39980eb1391832e813
V-SPOT <83084169+ajmalyaseen@users.noreply.github.com>
2021-05-04 18:15:33 +0530

    translation.py

--- translation.py
+++ translation.py
@@ -11,7 +11,7 @@ class Translation(object):
     RCHD_TG_API_LIMIT = "നീ കണ്ടോ ഞാൻ {} സെക്കന്റിൽ Download ചെയ്തു.\nFile Size ഞാൻ കണ്ടുപിടിച്ചു: {}\nSorry നീ എന്തുവാ വിചാരിച്ചേ! ഞാൻ ഇപ്പോൾ Upload ചെയ്യും? \n\n<b>"
     AFTER_SUCCESSFUL_UPLOAD_MSG = "**Rename ചെയ്യാൻ എന്നെ ഉപയോഗിച്ചതിന് നന്ദി അളിയാ, നന്ദി >**"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "നീ കണ്ടോ ഞാൻ {} സെക്കന്റിൽ Download ചെയ്തു.\nഎന്നിട്ട് {} സെക്കന്റിൽ Upload ചെയ്തു. \n\n<b>"
-    NOT_AUTH_USER_TEXT = "Contact: <b>@KL_noob</b>"
+    NOT_AUTH_USER_TEXT = "Contact: <b>@diago_x</b>"
     NOT_AUTH_USER_TEXT_FILE_SIZE = "ഈ മെസ്സേജ് നിനക്ക് കിട്ടിയെങ്കിൽ നീ <b>@KL_noob</b> മുതലാളിയെ Contact ചെയ്യണം."
     REPLY_TO_DOC_FOR_RENAME_FILE = "ഒരു ഫയലിനു Reply ആയി <code>/rename</code> <b>NewName.ext</b> എന്ന് തരുക അപ്പോൾ ഞാൻ ആ File Rename ചെയ്തു തരുന്നതാണ്. \n\n<b>"
     SAVED_CUSTOM_THUMB_NAIL = "ഞാൻ Custom Thumbnail Save ചെയ്തു ✅. ഈ Thumbnail ആയിരിക്കും ഇനി മുതൽ ഉള്ള എല്ലാ ഫയലുകൾക്കും ഞാൻ Use ചെയ്യുക. \n\n<b>"
@@ -23,7 +23,7 @@ class Translation(object):
     SHOW_CUSTOM_THUMB_NAIL = "<b>🔰Permanent Thumbnail🔰</b> \n\n<b>"
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
     KL35_THUMBNAIL_PHOTO = "https://telegra.ph/file/1ee9fd2c4b6a6b3c39262.jpg"
-    ABOUT_ME = """<b>🚫 group:__</b> \n\n<b>🔰 owner Name: </b> \n\n<b>🛡️ Creator: @KL_noob</b> \n\n<b>🔸 Language: Python3</b> \n\n<b>🔸 Library : <a href="https://docs.pyrogram.org/">Pyrogram asyncio 0.16.1</a></b> \n\n<b>🔸 Source Code : 👉 <a href="https://bit.ly/2AVFsoD">Click Here</a></b> \n\n<b>"""
+    ABOUT_ME = """<b>🚫 group:__</b> \n\n<b>🔰 owner Name: </b> \n\n<b>🛡️ Creator: @diago_x</b> \n\n<b>🔸 Language: Python3</b> \n\n<b>🔸 Library : <a href="https://docs.pyrogram.org/">Pyrogram asyncio 0.16.1</a></b> \n\n<b>🔸 Source Code : 🔐"""
     KL14_THUMBNAIL_PHOTO = "https://telegra.ph/file/1ee9fd2c4b6a6b3c39262.jpg"
     HELP_USER = """ഇത് അത്രക്ക് പാടൊന്നുമല്ലടെ.
     
